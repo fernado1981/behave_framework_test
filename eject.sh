9c381a6d0d2b337b154fde84f462694aa082d1e4
fernixq981 <fernando.manrique.villanueva@gmail.com>
2021-01-02 12:57:37 +0100

    update names_files and modified eject.sh

--- eject.sh
+++ eject.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
-#esto es una prueba
-behave -f allure_behave.formatter:AllureFormatter -o reports/allure/results ./features
-allure serve reports/allure/results
+
+# Delete any previous report files
+rm -f reports/allure/results/*.json
+
+# Run Every tuto
+for folder in features/orange_hrm/orange*; do
+	behave $folder -f json.pretty -o reports/allure/results/$folder.json || true;
+done
+
+# Convert every report to cucumber format for further jenkins report
+for f in reports/allure/results//* ; do
+	python convert2cucumber.py $f
+done
