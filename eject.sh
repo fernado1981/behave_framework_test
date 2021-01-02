30b1c1401fe4c8b119666ba5bd9d46a5863779ff
fernixq981 <fernando.manrique.villanueva@gmail.com>
2021-01-02 20:20:02 +0100

    update eject.sh

--- eject.sh
+++ eject.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Delete any previous report files
-rm -f reports/allure/results/*.json
+rm -f reports/allure/results/features/orange_hrm/*.json
 
 # Run Every feature
-for folder in features/orange_hrm/orange*; do
-	behave $folder -f json.pretty -o reports/allure/results/$folder.json || true;
+for folder in features/orange_hrm/*; do
+	behave $folder -f json.pretty -o reports/allure/results/features/orange_hrm/$folder.json || true;
 done
 
 cp -R reports/allure/results/features/orange_hrm/orange*.json reports/allure
