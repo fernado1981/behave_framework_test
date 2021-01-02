84b9e37ffac5810a3c83069705234a6bbe1584c8
fernixq981 <fernando.manrique.villanueva@gmail.com>
2021-01-02 18:00:02 +0100

    add webdriver_manager

--- web/factory.py
+++ web/factory.py
@@ -1,10 +1,11 @@
 from selenium import webdriver
-
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.firefox import FirefoxDriverManager
 from web.web import Web
 
 
 def get_web(browser):
     if browser == "chrome":
-        return Web(webdriver.Chrome())
+        return Web(webdriver.Chrome(ChromeDriverManager().install()))
     if browser == "firefox":
-        return Web(webdriver.Firefox())
+        return Web(webdriver.Firefox(FirefoxDriverManager().install()))
