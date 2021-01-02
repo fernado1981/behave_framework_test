dacce42f575a67267bdc51642ea9b2a080eaf820
fernixq981 <fernando.manrique.villanueva@gmail.com>
2021-01-02 18:15:35 +0100

    uopdate webdriver)

--- web/factory.py
+++ web/factory.py
@@ -1,11 +1,11 @@
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.firefox import FirefoxDriverManager
 from web.web import Web
 
 
 def get_web(browser):
     if browser == "chrome":
-        return Web(webdriver.Chrome(ChromeDriverManager().install()))
+        #return Web(webdriver.Chrome(ChromeDriverManager().install()))
+        return Web(webdriver.Chrome('web/drivers/chromedriver'))
     if browser == "firefox":
         return Web(webdriver.Firefox(FirefoxDriverManager().install()))
