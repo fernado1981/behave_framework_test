003747e1de4203e27d0e758c817bc4512b967ae9
fernixq981 <fernando.manrique.villanueva@gmail.com>
2021-01-03 13:02:51 +0100

    update eject.sh and delete cucumber2cumcumber.py

--- eject.sh
+++ eject.sh
@@ -3,13 +3,4 @@
 # Delete any previous report files
 rm -f reports/allure-results/*.json
 
-# Run Every tuto
-#for folder in features/orange_hrm/*; do
-  behave -f allure_behave.formatter:AllureFormatter -o reports/allure-results ./features
-  #behave $folder -f json.pretty -o reports/allure-results/$folder.json || true;
-#done
-
-# Convert every report to cucumber format for further jenkins report
-#for f in reports/allure/results/* ; do
- # python convert2cucumber.py $f
-#done
+behave -f allure_behave.formatter:AllureFormatter -o reports/allure-results ./features
